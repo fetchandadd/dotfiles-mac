a1deec93893a8676a0231a5bc15e7414ddafa95f
Tim Tegeler <tim@tegeler.me>
2016-09-27 08:26:47 +0200

    Modify PATH for homebrew

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,14 @@ set -gx OMF_PATH "/Users/tim/.local/share/omf"
 source $OMF_PATH/init.fish
 
 set -g fish_key_bindings fish_vi_key_bindings
+
+# NOTE: There is probably a sexier nicer way to do this, but until I figure that out I am manually unsetting here.
+# Unsets PATH
+set -g -x PATH
+
+# This allows us to use Homebrew versions of things (like git) rather than the pre-installed or XCode installed versions.
+# See http://blog.grayghostvisuals.com/git/how-to-keep-git-updated/ for reference.
+set -g -x PATH $PATH /usr/local/bin
+
+# Sets necessary PATH defaults
+set -g -x PATH $PATH /usr/bin /bin /usr/sbin /sbin
